0d5d8c5b01eb63822571773838e2807f7dc32d94
Alan <alan.f@live.com>
2022-12-06 21:51:17 -0300

    +Acceso a endpoints buscador

--- Front/JS/index.js
+++ Front/JS/index.js
@@ -1,13 +1,32 @@
 let allTrips = [];
 
-fetch("JS/trips.json")
+/*fetch("JS/trips.json")
 .then(res => res.json())
 .then(data => {
     data.forEach(e => {
         allTrips.push(e)
     });
     showTrips(allTrips)
-})
+})*/
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+
+fetch("https://viaggio.up.railway.app/api/v1/accommodations", requestOptions)
+  .then(response => response.text())
+  .then(result => {
+    const datos =(JSON.parse(result))
+   const datos2 = (datos.data.accommodations)
+   console.log(datos2)
+   datos2.forEach(e => {
+    allTrips.push(e)
+});
+showTrips(allTrips)
+  })
+ 
+  .catch(error => console.log('error', error));
 
 
 const cardContainer = document.querySelector(".card-container")
@@ -40,13 +59,14 @@ const showTrips = (miArray) => {
                     <div id="bootstrapCarousel" class="carousel slide h-100" data-ride="carousel">
                       <div class="carousel-inner h-100">
                         <div class="carousel-item h-100 active">
-                          <img src="${el.img}" class="d-block w-100" alt="Hotel Image">
+                          <img src="${el.accommodationImgs[0]
+                          }" class="d-block w-100" alt="Hotel Image">
                         </div>
                         <div class="carousel-item h-100">
-                          <img src="${el.img}" class="d-block w-100" alt="Hotel Image">
+                          <img src="${el.accommodationImgs[1]}" class="d-block w-100" alt="Hotel Image">
                         </div>
                         <div class="carousel-item h-100">
-                          <img src="${el.img}" class="d-block w-100" alt="Hotel Image">
+                          <img src="${el.accommodationImgs[2]}" class="d-block w-100" alt="Hotel Image">
                         </div>
                       </div>
                       <a class="carousel-control-prev" href="#bootstrapCarousel" role="button" data-slide="prev">
@@ -62,7 +82,7 @@ const showTrips = (miArray) => {
                   <div class="hotel-card_info p-4">
                     <div class="d-flex align-items-center mb-2">
                     <div "class="text-muted mb-2"><i class="fas fa-map-marker-alt"></i> ${el.city}, ${el.country}
-                    <h5 class="mb-0 mr-2">${el.nombreDeHospedaje}</h5>
+                    <h5 class="mb-0 mr-2">${el.title}</h5>
                     </div>
 
                       
@@ -72,14 +92,7 @@ const showTrips = (miArray) => {
                     <div class="d-flex justify-content-between align-items-end">
                       <div class="hotel-card_details">
                       
-                      <div>
-                      <i class="fa fa-star text-warning"></i>
-                      <i class="fa fa-star text-warning"></i>
-                      <i class="fa fa-star text-warning"></i>
-                      <i class="fa fa-star text-warning"></i>
-                      <i class="fa fa-star text-warning"></i>
-                    </div>
-                        <div class="mb-2"><span class="badge badge-primary">4.5</span> <a href="#!" class="text-muted">(245 ratings & 56 reviews)</a></div>
+                        <div class="mb-2"><span class="badge badge-primary">${el.rating}</span> <a href="#!" class="text-muted">(245 ratings & 56 reviews)</a></div>
                         <div class="amnities d-flex mb-3">
                           <img class="mr-2" src="/Front/Recursos/Cards/icons/desk-bell.svg" data-toggle="tooltip" data-placement="top" title="Desk bell" alt="Desk bell">
                           <img class="mr-2" src="/Front/Recursos/Cards/icons/single-bed.svg" data-toggle="tooltip" data-placement="top" title="Single Bed" alt="Single Bed">
@@ -87,9 +100,8 @@ const showTrips = (miArray) => {
                           <img class="mr-2" src="/Front/Recursos/Cards/icons/wifi.svg" data-toggle="tooltip" data-placement="top" title="Wifi" alt="Wifi">
                         </div>
                         <ul class="hotel-checklist pl-0 mb-0">
-                          <li><i class="fa fa-check text-success"></i> Lorem ipsum dolor</li>
-                          <li><i class="fa fa-check text-success"></i> Cras lectus purus, </li>
-                          <li><i class="fa fa-check text-success"></i> ornare eget congue</li>
+                          <li><i class="fa fa-check text-success"></i> ${el.reviews[0]}</li>
+                          <li><i class="fa fa-check text-success"></i> ${el.reviews[1]} </li>
                         </ul>
                       </div>
                       <div class="hotel-card_pricing text-center">
